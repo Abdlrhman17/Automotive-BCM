611256710ee40e38552fdc5d0714181369e7c16c
Abdlrhman <Abdlrhman@local>
2026-02-18 21:40:39 +0200

    Added Door state machine transitions

---
+++ src/APP/Door_StateMachine_interface.h
@@ -0,0 +1,39 @@
+#ifndef DOOR_STATEMACHINE_INTERFACE_H_
+#define DOOR_STATEMACHINE_INTERFACE_H_
+
+/* ============================================ */
+/*              INCLUDES                        */
+/* ============================================ */
+#include "StdTypes.h"
+#include "VehicleManager.h"
+#include "Events.h"
+
+
+/* ============================================ */
+/*          PUBLIC FUNCTION PROTOTYPES          */
+/* ============================================ */
+
+/**
+ * Initialize Door State Machine
+ * Set initial state (assume closed at startup)
+ */
+void Door_StateMachine_Init(void);
+
+/**
+ * Process door events
+ * Handle DOOR_OPEN_EVENT and DOOR_CLOSE_EVENT
+ */
+void Door_StateMachine_ProcessEvent(ecu_event_t event);
+
+/**
+ * Periodic update
+ */
+void Door_StateMachine_Update(void);
+
+/**
+ * Get current door state
+ */
+door_state_t Door_GetState(void);
+
+
+#endif /* DOOR_STATEMACHINE_INTERFACE_H_ */
---
+++ src/APP/Door_StateMachine_program.c
@@ -0,0 +1,54 @@
+/* ============================================ */
+/*              INCLUDES                        */
+/* ============================================ */
+#include "StdTypes.h"
+#include "Door_StateMachine_interface.h"
+#include "Events.h"
+#include "Trace.h"
+
+
+/* ============================================ */
+/*          PRIVATE VARIABLES                   */
+/* ============================================ */
+static door_state_t Global_current_door_state = DOOR_CLOSED;
+
+
+/* ============================================ */
+/*       PUBLIC FUNCTIONS IMPLEMENTATION        */
+/* ============================================ */
+
+void Door_StateMachine_Init(void)
+{
+	Global_current_door_state = DOOR_CLOSED;	
+	 TRACE_INFO(TRACE_DOOR, "Door SM Initialized - CLOSED");
+}
+
+void Door_StateMachine_ProcessEvent(ecu_event_t event)
+{
+	switch(event)
+	{
+		case DOOR_CLOSE_EVENT:
+		Global_current_door_state = DOOR_CLOSED;	// CLOSED
+		TRACE_INFO(TRACE_DOOR,"Door Closed");
+		break;
+		
+		case DOOR_OPEN_EVENT:
+		Global_current_door_state = DOOR_OPEN;		// OPENED
+		TRACE_INFO(TRACE_DOOR,"Door Opened");
+		break;
+		
+		default:
+		// Ignore
+		break;
+	}
+}
+
+void Door_StateMachine_Update(void)
+{
+	// TODO: If any door ajar timeout warning if wanted
+}
+
+door_state_t Door_GetState(void)
+{
+	return Global_current_door_state;
+}

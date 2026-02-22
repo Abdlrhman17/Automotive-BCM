b248c3719ba74d35e6e06855449d7a9cf6c902f3
Abdlrhman <Abdlrhman@local>
2026-02-22 22:14:49 +0200

    Added Vehicle Movement state machine transitions

---
+++ src/APP/VehicleMovement_StateMachine_interface.h
@@ -0,0 +1,37 @@
+#ifndef VEHICLEMOVEMENT_STATEMACHINE_INTERFACE_H_
+#define VEHICLEMOVEMENT_STATEMACHINE_INTERFACE_H_
+
+/* ============================================ */
+/*              INCLUDES                        */
+/* ============================================ */
+#include "VehicleManager.h"
+#include "Events.h"
+
+
+/* ============================================ */
+/*          PUBLIC FUNCTION PROTOTYPES          */
+/* ============================================ */
+
+/**
+ * Initialize Vehicle Movement State Machine
+ * Set initial state to STOPPED
+ */
+void VehicleMovement_StateMachine_Init(void);
+
+/**
+ * Process movement events
+ * Handle VEHICLE_STOPPED_EVENT and VEHICLE_MOVING_EVENT
+ */
+void VehicleMovement_StateMachine_ProcessEvent(ecu_event_t event);
+
+/**
+ * Periodic update
+ */
+void VehicleMovement_StateMachine_Update(void);
+
+/**
+ * Get current movement state
+ */
+vehicle_movement_state_t VehicleMovement_GetState(void);
+
+#endif /* VEHICLEMOVEMENT_STATEMACHINE_INTERFACE_H_ */
---
+++ src/APP/VehicleMovement_StateMachine_program.c
@@ -0,0 +1,47 @@
+/* ============================================ */
+/*              INCLUDES                        */
+/* ============================================ */
+#include "VehicleMovement_StateMachine_interface.h"
+#include "Trace.h"
+
+
+/* ============================================ */
+/*          PRIVATE VARIABLES                   */
+/* ============================================ */
+// Global Var to the state of the Car movement
+static vehicle_movement_state_t Global_current_movement_state = STOPPED;
+
+
+/* ============================================ */
+/*       PUBLIC FUNCTION IMPLEMENTATIONS        */
+/* ============================================ */
+
+void VehicleMovement_StateMachine_Init(void)
+{
+	Global_current_movement_state = STOPPED;
+	TRACE_INFO(TRACE_MOVEMENT,"Movement state Initialized to STOPPED");
+}
+
+void VehicleMovement_StateMachine_ProcessEvent(ecu_event_t event)
+{
+	if(event == VEHICLE_STOPPED_EVENT)
+	{
+		Global_current_movement_state = STOPPED;
+		TRACE_INFO(TRACE_MOVEMENT,"Movement state Set to STOPPED");
+	}
+	else if (event == VEHICLE_MOVING_EVENT)
+	{
+		Global_current_movement_state = MOVING;
+		TRACE_INFO(TRACE_MOVEMENT,"Movement state Set to MOVING");
+	}
+}
+
+void VehicleMovement_StateMachine_Update(void)
+{
+	// No periodic tasks needed
+}
+
+vehicle_movement_state_t VehicleMovement_GetState(void)
+{
+	return Global_current_movement_state;
+}

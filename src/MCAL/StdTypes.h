9667ad0b259f84b6531ee79a6f555f10b064ee2e
Abdlrhman <Abdlrhman@local>
2026-03-11 00:50:31 +0200

    Fixed types names

--- src/MCAL/StdTypes.h
+++ src/MCAL/StdTypes.h
@@ -12,27 +12,40 @@
 #define FALSE   0
 #endif
 
+#ifndef OK
+#define OK   1
+#endif
+
+#ifndef NOK
+#define NOK   0
+#endif
+
+#ifndef NULL
+#define NULL   0
+#endif
+
 #ifndef NULL_PTR
 #define NULL_PTR ((void*)0)
 #endif
+
 
 /* ============================
    Standard Integer Types
    ============================ */
 
 /* Unsigned types */
-typedef unsigned char      unit_8;      // 8-bit unsigned
-typedef unsigned short     unit_16;     // 16-bit unsigned
-typedef unsigned long      unit_32;     // 32-bit unsigned
-typedef unsigned long long unit_64;     // 64-bit unsigned
+typedef unsigned char      u8;      // 8-bit unsigned
+typedef unsigned short     u16;     // 16-bit unsigned
+typedef unsigned long      u32;     // 32-bit unsigned
+typedef unsigned long long u64;     // 64-bit unsigned
 
 /* Signed types */
-typedef signed char        sint_8;      // 8-bit signed
-typedef signed short       sint_16;     // 16-bit signed
-typedef signed long        sint_32;     // 32-bit signed
-typedef signed long long   sint_64;     // 64-bit signed
+typedef signed char        s8;      // 8-bit signed
+typedef signed short       s16;     // 16-bit signed
+typedef signed long        s32;     // 32-bit signed
+typedef signed long long   s64;     // 64-bit signed
 
 /* Character type */
-typedef char               char_8;      // 8-bit char
+typedef char               c8;      // 8-bit char
 
 #endif /* STD_TYPES_H_ */

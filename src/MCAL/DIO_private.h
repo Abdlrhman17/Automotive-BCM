4deb2f27d8d8cc7647af3ddc25a0a9a360a3d0ec
Abdlrhman <Abdlrhman@local>
2026-04-14 17:24:27 +0200

    Not really important

--- src/MCAL/DIO_private.h
+++ src/MCAL/DIO_private.h
@@ -3,7 +3,7 @@
 #define DIO_PRIVATE_H_
 
 extern const DIO_PinStatus_type PinStatusArray[TotalPins];
-static void DIO_InitPin(DIO_PinNum_type pin, DIO_PinStatus_type status);
+void DIO_InitPin(DIO_PinNum_type pin, DIO_PinStatus_type status);
 
 
 

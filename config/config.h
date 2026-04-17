b58d55c39f777cdfc64391da83acf9b51ccf8b3f
Abdlrhman <Abdlrhman@local>
2026-04-17 15:06:17 +0200

    Added defines for timing

--- config/config.h
+++ config/config.h
@@ -4,6 +4,10 @@
 // How many Latched Faults
 #define LATCHED_FAULTS_COUNT	3
 
+// State machine periodicity time
+#define SM_UPDATE_PERIOD_MS     50
 
+// Output controllers periodicity time
+#define OUTPUT_UPDATE_PERIOD_MS     10
 
 #endif /* CONFIG_H_ */

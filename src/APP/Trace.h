870b4154fe41b5ce251bb1748279bc4e3c645c53
Abdlrhman <Abdlrhman@local>
2026-04-29 15:53:35 +0300

    Added debuging trace & removed avr/pgmspace lib

--- src/APP/Trace.h
+++ src/APP/Trace.h
@@ -2,7 +2,15 @@
 #define TRACE_H_
 
 #include "UART_interface.h"
-#include <avr/pgmspace.h>
+
+#ifndef PROGMEM
+#define PROGMEM __attribute__((__progmem__))
+#endif
+
+#ifndef PSTR
+#define PSTR(s) (__extension__({static const char __c[] PROGMEM = (s); &__c[0];}))
+#endif
+
 
 typedef enum
 {
@@ -29,5 +37,25 @@ typedef enum
 #define TRACE_ERROR(cat, msg)  uart_send_string_P(PSTR("[ERR][" #cat "] " msg "\r\n"))
 #define TRACE_INFO(cat, msg)   uart_send_string_P(PSTR("[INF][" #cat "] " msg "\r\n"))
 #define TRACE_DEBUG(cat, msg)  uart_send_string_P(PSTR("[DBG][" #cat "] " msg "\r\n"))
+
+
+/*Trace with the possibility of sending variables */
+#define TRACE_INFO_I(cat, msg, val) do {                     \
+	uart_send_string_P(PSTR("[INF][" #cat "] " msg));        \
+	uart_send_int(val);                                      \
+	uart_send_string_P(PSTR("\r\n"));                        \
+} while (0)
+
+#define TRACE_ERROR_I(cat, msg, val) do {                    \
+	uart_send_string_P(PSTR("[ERR][" #cat "] " msg));        \
+	uart_send_int(val);                                      \
+	uart_send_string_P(PSTR("\r\n"));                        \
+} while (0)
+
+#define TRACE_DEBUG_I(cat, msg, val) do {                    \
+	uart_send_string_P(PSTR("[DBG][" #cat "] " msg));        \
+	uart_send_int(val);                                      \
+	uart_send_string_P(PSTR("\r\n"));                        \
+} while (0)
 
 #endif /* TRACE_H_ */
